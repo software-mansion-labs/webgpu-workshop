95b0b7de2beeb2358ee749ab072a81241c11c5c5
Iwo Plaza <iwoplaza@gmail.com>
2025-05-28 00:23:21 +0200

    Update step 2 on mobile

--- src/lessons/3_LavaLamp/steps/mobile/step2.tsx
+++ src/lessons/3_LavaLamp/steps/mobile/step2.tsx
@@ -18,8 +18,9 @@ export async function init(ref: React.RefObject<CanvasRef>) {
 runOnUI(() => {
   'worklet';
   const tgpu = requireUI('typegpu').default;
-  const { div, add, mix } = requireUI('typegpu/std');
+  const { abs, div, min, mix, pow, sign } = requireUI('typegpu/std');
   const d = requireUI('typegpu/data');
+  const { perlin3d } = requireUI('typegpu/noise');
 
   const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
   tgpu.initFromDevice({ device });
@@ -29,113 +30,54 @@ runOnUI(() => {
     format: presentationFormat,
     alphaMode: 'premultiplied',
   });
-  const hash = tgpu['~unstable'].fn([d.vec2f], d.f32)(
-    /* wgsl */ `(p: vec2<i32>) -> f32 {
-      let n = p.x + p.y * 57;
-      let f = f32((n << 13) ^ n);
-      return fract(sin(f) * 43758.5453);
-    }`,
-  );
-  const lerp = tgpu['~unstable'].fn([d.f32, d.f32, d.f32], d.f32)(
-    /* wgsl */ `(a: f32, b: f32, t: f32) -> f32 {
-      return a + t * (b - a);
-    }`,
-  );
-  const fade = tgpu['~unstable'].fn([d.f32], d.f32)(
-    /* wgsl */ `(t: vec2<f32>) -> vec2<f32> {
-      return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
-    }`,
-  );
-  const grad = tgpu['~unstable'].fn([d.f32], d.f32)(
-    /* wgsl */ `(p: vec2<i32>) -> vec2<f32> {
-      let h = hash(p) * 4.0;
-      let angle = 6.2831853 * fract(h); // 2π * hash
-      return vec2<f32>(cos(angle), sin(angle));
-    }`,
-  ).$uses({ hash });
-  const noise = tgpu['~unstable'].fn([d.vec2f], d.f32)(
-    /* wgsl */ `(p: vec2<f32>) -> f32 {
-      let pi = vec2<i32>(floor(p));
-      let pf = fract(p);
-      let w = fade(pf);
 
-      let a = grad(pi + vec2<i32>(0, 0));
-      let b = grad(pi + vec2<i32>(1, 0));
-      let c = grad(pi + vec2<i32>(0, 1));
-      let d = grad(pi + vec2<i32>(1, 1));
+  const sharpen = tgpu['~unstable'].fn([d.f32], d.f32)((value) => {
+    return sign(value) * pow(abs(value), 0.6);
+  });
 
-      let va = dot(a, pf - vec2(0.0, 0.0));
-      let vb = dot(b, pf - vec2(1.0, 0.0));
-      let vc = dot(c, pf - vec2(0.0, 1.0));
-      let vd = dot(d, pf - vec2(1.0, 1.0));
-
-      let x1 = lerp(va, vb, w.x);
-      let x2 = lerp(vc, vd, w.x);
-      return lerp(x1, x2, w.y);
-    }`,
-  ).$uses({ hash, lerp, fade, grad });
-  
-  const mainVertex = tgpu['~unstable'].vertexFn({
-    in: { vertexIndex: d.builtin.vertexIndex },
-    out: { outPos: d.builtin.position },
+  const fullScreenTriangle = tgpu['~unstable'].vertexFn({
+    in: { idx: d.builtin.vertexIndex },
+    out: { pos: d.builtin.position },
   })((input) => {
-    'kernel';
-    const pos = [
-      d.vec2f(-1.0, -1.0),
-      d.vec2f(1.0, -1.0),
-      d.vec2f(-1.0, 1.0),
-      d.vec2f(-1.0, 1.0),
-      d.vec2f(1.0, -1.0),
-      d.vec2f(1.0, 1.0),
-    ];
+    const pos = [d.vec2f(-1, -1), d.vec2f(3, -1), d.vec2f(-1, 3)];
+
     return {
-      outPos: d.vec4f(pos[input.vertexIndex], 0.0, 1.0),
+      pos: d.vec4f(pos[input.idx], 0, 1),
     };
   });
 
-  const timeUniform = root.createBuffer(d.f32, 0).$usage('uniform');
-  const bindGroupLayout = tgpu.bindGroupLayout({
-    time: { uniform: d.f32 },
-  });
+  const timeUniform = root['~unstable'].createUniform(d.f32, 0);
 
   const mainFragment = tgpu['~unstable'].fragmentFn({
     in: { pos: d.builtin.position },
     out: d.vec4f,
   })((input) => {
-    'kernel';
-    const time = bindGroupLayout.$.time;
-    const uv = div(input.pos.xy, d.vec2f(width * 0.2, height * 0.1));
-    const p = add(uv, div(d.vec2f(time, time), 3000));
-    const n = noise(p);
-
+    const minDim = d.f32(min(width, height));
+    const uv = div(input.pos.xy, minDim * 0.2);
+    const time = timeUniform.value;
+    const n = sharpen(perlin3d.sample(d.vec3f(uv, time)));
     const color = mix(
       div(d.vec4f(153, 0, 105, 255), 255),
       div(d.vec4f(255, 140, 26, 255), 255),
       n * 0.5 + 0.5,
     );
     return color;
-  }).$uses({ noise });
+  });
 
   const pipeline = root['~unstable']
-    .withVertex(mainVertex, {})
+    .withVertex(fullScreenTriangle, {})
     .withFragment(mainFragment, { format: presentationFormat })
     .createPipeline();
-
-  const bindGroup = root.createBindGroup(bindGroupLayout, {
-    time: timeUniform,
-  });
 
   function frame() {
     timeUniform.write(performance.now() * 0.0002 % 10);
     pipeline
       .withColorAttachment({
         view: context.getCurrentTexture().createView(),
-        clearValue: [0, 0, 0, 0],
         loadOp: 'clear',
         storeOp: 'store',
       })
-      .with(bindGroupLayout, bindGroup)
-      .draw(6);
+      .draw(3);
     context.present();
     requestAnimationFrame(frame);
   }
